813042d29e979c3f979b3114c997d55443828833
Kermit <254563235+kermitthefroggs@users.noreply.github.com>
2026-01-14 20:45:46 +1300

    Implemented the house, a gridmap, psx filter, entire basic interior, funny secrets :3

---
+++ personal_virtual_house - v1.0/scripts/illusion_breaker.gd
@@ -0,0 +1,7 @@
+extends Area3D
+
+@export var player : CharacterBody3D
+
+func _on_body_entered(_body: Node3D) -> void:
+	print("behind. you. AHHHHHHHHHH!!!!! SO SCARY!!!!!!!!!")
+	print("command line jumpscare>!>!>!>!!>")
