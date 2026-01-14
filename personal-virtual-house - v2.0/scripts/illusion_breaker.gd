30281bad997e279a19f32be4d4bce25861116b51
Kermit <254563235+kermitthefroggs@users.noreply.github.com>
2026-01-15 10:51:54 +1300

    Just moving files around to organise

--- personal-virtual-house - v2.0/scripts/illusion_breaker.gd
+++
@@ -1,7 +0,0 @@
-extends Area3D
-
-@export var player : CharacterBody3D
-
-func _on_body_entered(_body: Node3D) -> void:
-	print("behind. you. AHHHHHHHHHH!!!!! SO SCARY!!!!!!!!!")
-	print("command line jumpscare>!>!>!>!!>")
--- personal_virtual_house - v1.0/scripts/camera_main.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-func _ready() -> void:
-	Engine.max_fps = 30
